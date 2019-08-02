21595b079dd57d933a8620da985249f8d5cfa776
zc <1041634962@qq.com>
2019-08-02 14:08:46 +0800

    firstcommit

---
+++ demo01/src/main/java/com/it/User.java
@@ -0,0 +1,40 @@
+package com.it;
+
+/**
+ * Package: com.it
+ * Author: 张成
+ * Date: Created in 2019/8/2 13:50
+ **/
+public class User {
+    private String name;
+    private String age;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
